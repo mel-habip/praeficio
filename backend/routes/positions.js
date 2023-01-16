9b3c6f8cb6f4e0cb8d863f761f956507877992a6
Mel <Mel@makofintech.com>
2023-01-16 08:23:25 -0500

    Continued working on logic for Positions

--- backend/routes/positions.js
+++ backend/routes/positions.js
@@ -4,9 +4,8 @@ const log = console.log;
 
 import authenticateToken from '../jobs/authenticateToken.js';
 import authenticateUser from '../jobs/authenticateUser.js';
-import {
-    defaultPermissions
-} from '../constants/defaultPermissions.js';
+import defaultPermissions from '../constants/defaultPermissions.js';
+import connection from '../utils/db_connection.js';
 
 positionRouter.use(authenticateToken);
 positionRouter.use(authenticateUser);
@@ -14,7 +13,7 @@ positionRouter.use(authenticateUser);
 
 
 positionRouter.get('/:position_id', (req, res) => {
-    let sql = `SELECT ` //TODO: finish this, check a couple things:
+    let sql = `SELECT ${req.params.position_id} FROM Positions` //TODO: finish this, check a couple things:
 
     //check if query includes 'include-deactivated:true', else filter them out
 
@@ -28,16 +27,41 @@ positionRouter.get('/:position_id', (req, res) => {
 });
 
 positionRouter.post('/', (req, res) => {
-    let sql = ` ` //TODO: create new position for user prescribed in the data. 
+
+    if (req.user.id !== req.body.user_id && !defaultPermissions.actions.create_positions_for_others.includes(req.user.Permissions)) {
+        res.status(403).send(`Forbidden: ${req.user.Permissions} cannot create Position for non-self.`)
+    }
+
+    let sql = `INSERT INTO Positions (UserID, Ticker, AcquiredOn, SoldOn) VALUES '${req.body.user_id}', '${req.body.ticker}', '${req.body.acquired_on}', '${req.body.sold_on}'`;
+
+
+
     //TODO: check user permissions to see if they are allowed to create positions for folk other than themselves
 });
 
 positionRouter.post('/import', (req, res) => {
-    let sql = `SELECT ` //TODO: finish this, provide import modes as CSV, XML or JSON in prescribed formats
+
+    //work the file provided some how
+    const provided_data = []; //TODO: provide import modes as CSV, XML or JSON in prescribed formats
+
+    let sql = `INSERT INTO Positions (UserID, Ticker, AcquiredOn, SoldOn) ` + provided_data.map(row => `('${row.user_id}', '${row.ticker}', '${row.acquired_on}', '${row.sold_on}')`).join(', ') + ';';
+
+
 });
 
 positionRouter.post('/export', (req, res) => {
-    let sql = `SELECT ` //TODO: finish this, provide as a CSV... I know how to prepare but not sure about making the FE download
+
+    let sql = `SELECT * FROM Positions` //TODO: finish this, provide as a CSV... I know how to prepare but not sure about making the FE download;
+
+    let result = [];
+
+    //implement Json logic from --> https://jsonlogic.com/ ? Or build your own? I don't like this one too much and can build a simplified version of it myself
+    //consider a mechanism whereby we do the filter at the SQL level instead of JS. In theory this should reduce Server Memory usage? 
+
+    let filtered_result = result.filter(position => condition_evaluator(req.body.filter_rule, position));
+
+    res.status(200).json(filtered_result);
+
 });
 
 
