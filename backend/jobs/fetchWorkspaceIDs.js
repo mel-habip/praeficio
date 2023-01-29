fee1a01f777ab359fbd68aaf86dc5dfa1e1d6648
Mel <Mel@makofintech.com>
2023-01-29 16:48:55 -0500

    Update fetchWorkspaceIDs.js

--- backend/jobs/fetchWorkspaceIDs.js
+++ backend/jobs/fetchWorkspaceIDs.js
@@ -1,22 +1,19 @@
-import connection from '../utils/db_connection.js';
+import query from '../utils/db_connection.js';
 
-export default async function fetchPermission(id) {
-    let sql = `SELECT * FROM Users WHERE ${(isNaN(id)) ? 'Username': 'UserID'} = '${id}'`;
-    return connection.query(sql, (err, result) => {
-        if (err) throw err;
-        if (!result) {
-            throw Error(`User ${id} not found.`);
-        };
-        if (!result.workspace_ids) {
-            return [];
-        }
-        let res = JSON.parse(result.workspace_ids);
-        
-    });
 /**
  * @async_function fetchWorkspaces - checks DB for Workspaces to which this user is related
  * @param {String|Number} id - ID of the user to check
  * @param {Array<String|Number>} only_display - filters the result down to exclude IDs not in this array, if provided
  * @returns {Promise<Array<Number>>} array of WorkspaceIDs
  */
+export default async function fetchWorkspaces(id, only_display = null) {
+    let sql = `SELECT WorkspaceID FROM WorkspaceAssociations WHERE UserID = ?`;
+
+    let result = await query(sql, id);
+
+    if (only_display && Array.isArray(only_display) && only_display.length) {
+        result?.map(res => res.WorkspaceID)?.filter(w_id => only_display.includes(w_id)) || [];
+    }
+
+    return result?.map(res => res.WorkspaceID) || [];
 };
