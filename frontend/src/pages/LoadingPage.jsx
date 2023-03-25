b8297b45b62ce095af973827e48b4bf49cc9b717
Mel <Mel@makofintech.com>
2023-03-24 23:11:37 -0400

    Created a LoadingPage based on a tutorial

---
+++ frontend/src/pages/LoadingPage.jsx
@@ -0,0 +1,10 @@
+
+import './LoadingPage.css';
+
+export default function LoadingPage() {
+
+
+    return (<body className="loading-page-body">
+        <h1 className="loading-page-text" data-text="Loading..." >Loading...</h1>
+    </body>);
+}
