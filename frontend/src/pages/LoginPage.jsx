5973787173feaaa56d5852a3a412d771a27d9646
Mel <Mel@makofintech.com>
2023-05-11 10:48:24 -0400

    Update LoginPage.jsx

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -132,6 +132,7 @@ export default function LoginPage() {
                                             setAccessToken(res.access_token);
                                             setUserId(res.user_id);
                                             setIsLoggedIn(true);
+                                            window.location.replace('/portal');
                                         } else {
                                             catchError(res)
                                         }
