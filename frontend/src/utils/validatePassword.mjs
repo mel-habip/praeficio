ed71ca2b7ee6c30bc359480cafed3275c49ba492
Mel <Mel@makofintech.com>
2023-05-17 19:29:00 -0400

    Re-did the password strength validations

--- frontend/src/utils/validatePassword.mjs
+++ frontend/src/utils/validatePassword.mjs
@@ -1,6 +1,18 @@
+const uppercaseRegex = /[A-Z]/;
+const lowercaseRegex = /[a-z]/;
+const numberRegex = /[0-9]/;
+const symbolRegex = /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/;
 
-export default function validatePassword(password = '') {
+export default function validatePassword(string = '') {
     //requires a number, an uppercase, a lowercase and a special character, min 6, max 15
-    const rule = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!?@()_\-+{}|[\]'#=.$%^&*])[a-zA-Z0-9!?@()_\-+{}|[\]'#=.$%^&*]{6,15}$/;
-    return !!password.match(rule);
+    if (string.length < 6) return false;
+    if (string.length > 15) return false;
+
+    //technically this setup means we keep testing even though it failed already, but its too little of a gain to optimize further
+    const hasUppercase = uppercaseRegex.test(string);
+    const hasLowercase = lowercaseRegex.test(string);
+    const hasNumber = numberRegex.test(string);
+    const hasSymbol = symbolRegex.test(string);
+
+    return hasUppercase && hasLowercase && hasNumber && hasSymbol;
 }
