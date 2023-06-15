66833b10e7583b929c41c634c9ab2244fabd0da9
Mel <Mel@makofintech.com>
2023-06-15 18:59:27 -0400

    small stylistic change & clearing mechanism

--- frontend/src/pages/QuickNotes.jsx
+++ frontend/src/pages/QuickNotes.jsx
@@ -21,9 +21,9 @@ function OneQuestionOneAnswer({ numOfSections, setNumOfSections, iteration }) {
     const [noteValue, setNoteValue] = useState('');
     return (
         <div style={{ width: '85%', display: 'flex', flexDirection: 'column', flexWrap: 'wrap', alignContent: 'center' }} >
-            {!!iteration && <Button onPress={() => setNumOfSections(x => x - 1)} auto shadow bordered color="error" css={{ margin: '15px', alignSelf: 'end' }} > &nbsp; <i className="fa-solid fa-trash-can" /> &nbsp;</Button>}
+            {!!iteration && <Button onPress={() => { localStorage.removeItem(`praeficio-note-q${iteration}`); localStorage.removeItem(`praeficio-note-a${iteration}`); setNumOfSections(x => x - 1); }} auto shadow bordered color="error" css={{ margin: '15px', alignSelf: 'end' }} > &nbsp; <i className="fa-solid fa-trash-can" /> &nbsp; <i className="fa-solid fa-angles-down"/> &nbsp;</Button>}
             <Textarea
-                labelPlaceholder={`Your question #${iteration}`}
+                labelPlaceholder={`Your question #${iteration+1}`}
                 bordered
                 width="90%"
                 color='primary'
@@ -42,7 +42,8 @@ function OneQuestionOneAnswer({ numOfSections, setNumOfSections, iteration }) {
                 initialValue={localStorage.getItem(`praeficio-note-a${iteration}`)}
                 onChange={e => setNoteValue(e.target.value) || localStorage.setItem(`praeficio-note-a${iteration}`, e.target.value)}
                 minRows={15} />
-            <Button onPress={() => setNumOfSections(x => x + 1)} auto shadow bordered css={{ margin: '15px', alignSelf: 'end' }} > &nbsp; <i className="fa-regular fa-square-plus" /> &nbsp; </Button>
+            {numOfSections-1 === iteration && <Button onPress={() => setNumOfSections(x => x + 1)} auto shadow bordered css={{ margin: '15px', alignSelf: 'end' }} > &nbsp; <i className="fa-regular fa-square-plus" /> &nbsp; </Button>}
+            
         </div>
     )
 }
